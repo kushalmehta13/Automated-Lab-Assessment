33b09d50d8e39fab285448d048c99d4f8fb7c12d
maheshp2812 <maheshp2812@gmail.com>
2017-11-18 13:30:59 +0530

    Fixed indentations

--- routes/index.js
+++ routes/index.js
@@ -29,18 +29,18 @@ router.get('/code', function(req, res, next) {
   });
 
 router.post('/studentDashboard' , function(req, res, next) {
-	//Login details are check with DB and then only page is rendered
+	//Login details are checked with DB and then only page is rendered
 	res.render('studentDashboard', { title: 'Solve' });
 });
 
 router.post('/teacherDashboard' , function(req, res, next) {
-	//Login details are check with DB and then only page is rendered
+	//Login details are checked with DB and then only page is rendered
 	res.render('teacherDashboard', { title: 'Solve' });
 });
 
 router.post('/compilecode', function(req, res, next) {
   	var code = req.body.code;
-	var input = req.body.input;
+    var input = req.body.input;
     var inputRadio = req.body.inputRadio;
     var lang = req.body.lang;
 
@@ -53,37 +53,21 @@ router.post('/compilecode', function(req, res, next) {
       });
     });
 
-    if(inputRadio === "true")
-        {
-        	var envData = { OS : "linux" , cmd : "gcc"};
-        	compiler.compileCPPWithInput(envData , code ,input , function (data) {
-        		if(data.error)
-        		{
-        			res.send(data.error);
-        		}
-        		else
-        		{
-        			res.send(data.output);
-        		}
-        	});
-	   }
-	   else
-	   {
-
-	   	var envData = { OS : "linux" , cmd : "gcc"};
-        	compiler.compileCPP(envData , code , function (data) {
-        	if(data.error)
-        	{
-        		res.send(data.error);
-        	}
-        	else
-        	{
-        		res.send(data.output);
-        	}
-
-            });
-	   }
-
+    // Compile code and send output
+    if(inputRadio === "true") {
+      	var envData = { OS : "linux" , cmd : "gcc"};
+      	compiler.compileCPPWithInput(envData , code ,input , function (data) {
+        	if(data.error) res.send(data.error);
+        	else res.send(data.output);
+        });
+    }
+    else {
+    	var envData = { OS : "linux" , cmd : "gcc"};
+    	compiler.compileCPP(envData , code , function (data) {
+      	if(data.error) res.send(data.error);
+      	else res.send(data.output);
+      });
+    }
 });
 
 module.exports = router;
