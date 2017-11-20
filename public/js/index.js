8973b925a2d3fe5e8bb9008dfd5aeff69f079a38
Imran Ahmed Manzoor <imran.manzoor31@gmail.com>
2017-11-20 19:23:55 +0530

    Fixed editor: results can be seen in console and page

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,18 @@
-function submitCode() {
-  const code = $('#code').val();
+function submitCode(element,out) {
+  const code = element.getValue();
   const inputRadioVal = $('input[name=inputRadio]:checked').val();
   const input = $('#input').val();
   const lang = $('#lan').val();
 
   $.post('/compilecode', {
     'code' : code,
-    'input' : input,
-    'inputRadio' : inputRadioVal,
-    'lang' : lang
+    'input' : 'No',
+    'inputRadio' : 'No',
+    'lang' : 'C'
   }).done((output) => {
     // TODO: format and display the output as needed
+    console.log(out);
+    out.html(output);
     console.log(output);
   });
 }
