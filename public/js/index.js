9392756dd60d78ceab763d16035447b1feede6dd
Imran Ahmed Manzoor <imran.manzoor31@gmail.com>
2017-11-20 21:30:59 +0530

    Fixed language selection

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,14 @@
-function submitCode(element,out) {
+function submitCode(element,out, langselect) {
   const code = element.getValue();
   const inputRadioVal = $('input[name=inputRadio]:checked').val();
   const input = $('#input').val();
-  const lang = $('#lan').val();
+  const lang = langselect.value;;
 
   $.post('/compilecode', {
     'code' : code,
     'input' : 'No',
     'inputRadio' : 'No',
-    'lang' : 'C'
+    'lang' : lang
   }).done((output) => {
     // TODO: format and display the output as needed
     console.log(out);
